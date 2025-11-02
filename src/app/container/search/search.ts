81b185266f631ec3a6a465c4f655f54abd38b873
MATHAN1241 <mathan03032002@gmail.com>
2025-11-02 13:25:01 +0530

    angular Ngclass NgIf NgStyle

--- src/app/container/search/search.ts
+++ src/app/container/search/search.ts
@@ -1,15 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './search.html',
   styleUrl: './search.css',
 })
 export class Search {
-   searchText: string= "MensWear";
-
+  // searchText: string= "MensWear";
+  searchText: string = '';
   //  searchTextChange(event:any){
   //    this.searchText=event.target.value;
   //  }
