e31773b9897891ca2ae5827196f65e3a80f87dca
MATHAN1241 <mathan03032002@gmail.com>
2025-11-19 09:29:19 +0530

    ng-content learn

--- src/app/container/container.ts
+++ src/app/container/container.ts
@@ -3,10 +3,11 @@ import { Search } from './search/search';
 import { CommonModule } from '@angular/common';
 import { ProductList } from './product-list/product-list';
 import { ProductDetail } from './product-detail/product-detail';
+import { FeaturedBrands } from './featured-brands/featured-brands';
 
 @Component({
   selector: 'container',
-  imports: [Search,CommonModule,ProductList,ProductDetail],
+  imports: [Search,CommonModule,ProductList,ProductDetail,FeaturedBrands],
   templateUrl: './container.html',
   styleUrl: './container.css',
 })
